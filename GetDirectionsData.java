8cd9bc5fa3af61cab0ad47e4d4a90b6075034c56
shahbazsharif786 <57809855+shahbazsharif786@users.noreply.github.com>
2019-11-15 23:28:23 +0500

    Drive

---
+++ GetDirectionsData.java
@@ -0,0 +1,82 @@
+package com.example.uttam.driver_behaviour;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class GetDirectionsData extends AsyncTask<Object,String,String> {
+
+    GoogleMap mMap;
+    String url;
+    String googleDirectionsData;
+    String duration, distance;
+    LatLng latLng;
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+        latLng = (LatLng)objects[2];
+
+
+
+        DownloadUrl downloadUrl = new DownloadUrl();
+        try {
+            googleDirectionsData = downloadUrl.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleDirectionsData;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        HashMap<String, String> distDirec = null;
+        String[] directionsList;
+        DataParser parser = new DataParser();
+        directionsList = parser.parseDirections(s);
+        displayDirection(directionsList);
+        distDirec = parser.DistAndDir(s);
+        duration = distDirec.get("duration");
+        distance = distDirec.get("distance");
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Duration :"+duration);
+        markerOptions.snippet("Distance "+distance);
+        mMap.addMarker(markerOptions);
+
+    }
+
+    public void displayDirection(String[] directionsList)
+    {
+
+        int count = directionsList.length;
+        for(int i = 0;i<count;i++)
+        {
+            PolylineOptions options = new PolylineOptions();
+            options.color(Color.RED);
+            options.width(10);
+            options.addAll(PolyUtil.decode(directionsList[i]));
+
+            mMap.addPolyline(options);
+        }
+    }
+
+
+
+
+
+
+}
+
---
+++ ViewDatabase.java
@@ -0,0 +1,95 @@
+package com.example.uttam.driver_behaviour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Uttam on 11/13/17.
+ */
+
+public class ViewDatabase extends AppCompatActivity {
+    private static final String TAG = "ViewDatabase";
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private ListView mListView;
+    private List<String> list;
+    ArrayAdapter adapter;
+    int counter = 0;
+    String UName;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_database_layout);
+        Intent i = getIntent();
+        UName = i.getStringExtra("userid");
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference("Users").child(UName).child("Sessions");
+        mListView =(ListView)findViewById(R.id.list_View);
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter(getApplicationContext(),R.layout.listitem,R.id.textView,list);
+        mListView.setAdapter(adapter);
+
+        /*myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                counter++;
+                String s0 = dataSnapshot.child("0").getValue().toString();
+                String s7 = dataSnapshot.child("7").getValue().toString();
+                String s8 = dataSnapshot.child("8").getValue().toString();
+
+                list.add("              Session: "+counter);
+                list.add(s8);
+                list.add(s0);
+                list.add(s7);
+                adapter.notifyDataSetChanged();
+                //Toast.makeText(getApplicationContext(),list.toStringText(),Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
